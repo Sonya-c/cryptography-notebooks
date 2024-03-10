e11b7ded124c995650032dd7c80a672bd8ef43b3
Sonya <sonya.castro.gomez@gmail.com>
2024-03-10 17:30:15 -0500

    chore

--- notebooks/scripts/utils.py
+++ notebooks/scripts/utils.py
@@ -22,33 +22,41 @@ def int_to_text(integer: int) -> str:
 assert("expa" == int_to_text(0x61707865)[::-1]), "Error"
 
 
-# Chatgpt assited generated code 
-# Do no ask me how it works
+def split_words(n: int, word_size: int, word_num: int) -> List[int]:
+    """Dvidide un número en palabras
 
-def get_words(value: int,
-             word_size: int,
-             words_num: int) -> List[int]:
-    
-    hex_string = hex(value)[2:]  # Remove '0x' prefix
-    
-    binary_string = bin(int(hex_string, 16))[2:].zfill(len(hex_string) * 4)  # Convert to binary
-    padding_bits = word_size * words_num - len(binary_string)
-    
-    if padding_bits < 0:
-        raise ValueError("Number of words specified is insufficient for the given value.")
-    
-    binary_string = binary_string.zfill(word_size * words_num)  # Add leading zeros for padding
-    
-    words = [int(binary_string[i:i+word_size], 2) for i in range(0, len(binary_string), word_size)]
-    
-    return words
+    Args:
+        n (int): _description_
+        word_size (int): tamaño de la palabra en bytes
+        word_num (int): cantidad de palabras
 
-assert(get_words(0x112233445566778899aabbccddeeff00, 32, 8) == [0x0, 0x0, 0x0, 0x0, 0x11223344, 0x55667788, 0x99aabbcc, 0xddeeff00]), "Error"
+    Returns:
+        List[int]: Lista de palabras
+    """
+    size = word_size * word_num
+    
+    n = n.to_bytes(size, byteorder='big')
+    return [int.from_bytes(n[i:i+word_size], byteorder='big') for i in range(0, len(n), word_size)]
+
+assert(split_words(0x1A38B5EE, 1, 4) == [0x1A, 0x38, 0xB5, 0xEE])
+assert(split_words(0x2b7e151628aed2a6abf7158809cf4f3c, 4, 4) == [0x2b7e1516, 0x28aed2a6, 0xabf71588, 0x09cf4f3c])
+assert(split_words( 0x8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b, 4, 6) == [0x8e73b0f7, 0xda0e6452, 0xc810f32b, 0x809079e5, 0x62f8ead2, 0x522c6b7b])
+assert(split_words(0x603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4, 4, 8) == [0x603deb10, 0x15ca71be, 0x2b73aef0, 0x857d7781, 0x1f352c07, 0x3b6108d7, 0x2d9810a3, 0x0914dff4])
 
 
-def from_words(words: List[int], word_size: int) -> int:
-    binary_string = ''.join(format(word, f'0{word_size}b') for word in words)
-    hex_string = hex(int(binary_string, 2))
-    return int(hex_string, 16)
+def join_words(words: List[int], word_size: int) -> int:
+    """Una una lista de palabras en un solo número
 
-assert(0x112233445566778899aabbccddeeff00 == from_words([0x0, 0x0, 0x0, 0x0, 0x11223344, 0x55667788, 0x99aabbcc, 0xddeeff00], 32)), "Error"
+    Args:
+        words (List[int]): Lista de palabras
+        word_size (int): tamaño de la palabra (en bytes)
+
+    Returns:
+        int: Entero
+    """
+    return int("".join([w.to_bytes(word_size, "big").hex() for w in words]), 16)
+
+assert(join_words([0x1A, 0x38, 0xB5, 0xEE], 1) == 0x1A38B5EE)
+assert(join_words([0x2b7e1516, 0x28aed2a6, 0xabf71588, 0x09cf4f3c], 4) == 0x2b7e151628aed2a6abf7158809cf4f3c)
+assert(join_words([0x8e73b0f7, 0xda0e6452, 0xc810f32b, 0x809079e5, 0x62f8ead2, 0x522c6b7b], 4) == 0x8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b)
+assert(join_words([0x603deb10, 0x15ca71be, 0x2b73aef0, 0x857d7781, 0x1f352c07, 0x3b6108d7, 0x2d9810a3, 0x0914dff4], 4) == 0x603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4)
