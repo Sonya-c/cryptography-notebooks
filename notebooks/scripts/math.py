e393021df23fd11c7cc68548d1d313f2e885dba5
Sonya <sonya.castro.gomez@gmail.com>
2024-03-10 20:57:31 -0500

    I don't understand mode of operation

--- notebooks/scripts/math.py
+++ notebooks/scripts/math.py
@@ -7,14 +7,3 @@ def sum32(a: int, b: int) -> int:
 
 assert(sum32(2**32, 3) == 3), "Error"
 
-
-def rot(w: int, r: int) -> int: 
-    """Rotate lest for 32 bits
-
-    Args:
-        w: word to rotate
-        r: I dont remember
-    """
-
-    mask = 0xffffffff
-    return ((w << r) & mask) | (w >> (32 - r)) 
